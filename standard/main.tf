6737391df377c820ebddd8615279e98699d63740
Fabiano Honorato <fabianoshz@gmail.com>
2024-10-11 18:27:00 -0300

    Test concurrency 3

--- standard/main.tf
+++ standard/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    time = {
+      source = "hashicorp/time"
+      version = "0.12.1"
+    }
+  }
+}
+
 resource "time_sleep" "sleep" {
   create_duration = "60s"
 }
